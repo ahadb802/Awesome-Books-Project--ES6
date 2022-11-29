c401379414aa1ae095c273f011dd698cafdf271e
PrangonGhose <prangonghose@gmail.com>
2022-11-30 03:05:40 +0600

    Add display and form functionalities

--- script.js
+++ script.js
@@ -31,3 +31,75 @@ class Todo {
     this.author = author;
   }
 }
+
+// display the todo in the DOM;
+class UI {
+  static displayData() {
+    const displayData = todoArr.map((item) => `
+            <div class="todo">
+            <div>
+            <span>"${item.title}" by</span>
+            <span> ${item.author}</span>
+            </div>
+            <button class="remove" data-id = ${item.id}>Remove</button>
+            </div>
+            `);
+    lists.innerHTML = (displayData).join(' ');
+  }
+
+  static colorChanger() {
+    const todos = document.querySelectorAll('.todo');
+    let count = 1;
+    todos.forEach((item) => {
+      if (count % 2 !== 0) {
+        item.classList.add('active');
+      } else if (item.classList.contains('active')) {
+        item.classList.remove('active');
+      }
+      count += 1;
+    });
+  }
+
+  static clearInput() {
+    bookTitle.value = '';
+    bookAuthor.value = '';
+  }
+
+  static removeTodo() {
+    lists.addEventListener('click', (e) => {
+      if (e.target.classList.contains('remove')) {
+        e.target.parentElement.remove();
+      }
+      const btnId = e.target.dataset.id;
+      // remove from array.
+      UI.removeArrayTodo(btnId);
+    });
+  }
+
+  static removeArrayTodo(id) {
+    todoArr = todoArr.filter((item) => item.id !== +id);
+    Storage.addTodStorage(todoArr);
+    UI.colorChanger();
+  }
+}
+
+// form part
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const id = Math.random() * 1000000;
+  const todo = new Todo(id, bookTitle.value, bookAuthor.value);
+  todoArr = [...todoArr, todo];
+  UI.displayData();
+  UI.colorChanger();
+  UI.clearInput();
+  // add to storage
+  Storage.addTodStorage(todoArr);
+});
+
+// once the browser is loaded
+window.addEventListener('DOMContentLoaded', () => {
+  UI.displayData();
+  UI.colorChanger();
+  // remove from the dom
+  UI.removeTodo();
+});
